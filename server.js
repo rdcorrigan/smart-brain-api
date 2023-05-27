9604b97e31a90014282d7cd18b6f12f6172e6a97
Ryan Corrigan <ryandcorrigan@gmail.com>
2023-05-27 13:51:08 -0600

    adding process.env.DATABASE variables

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      host : process.env.DATABASE_HOST,
       port: 5432,
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
 });
 
@@ -24,13 +26,27 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
+
+// Render backend live URL: 
+// https://smartbrainbackend-371k.onrender.com
+
+// PSQL Command: PGPASSWORD=EhvieZAPy7XTc7b6Q13kyjLdhCZOv3xG psql -h dpg-chogoevdvk4goeo0pheg-a.oregon-postgres.render.com -U smartbraindb_k5ws_user smartbraindb_k5ws
+
+// Password:
+// EhvieZAPy7XTc7b6Q13kyjLdhCZOv3xG
+
+// Internal Database URL:
+// postgres://smartbraindb_k5ws_user:EhvieZAPy7XTc7b6Q13kyjLdhCZOv3xG@dpg-chogoevdvk4goeo0pheg-a/smartbraindb_k5ws
+
+// External Database URL:
+// postgres://smartbraindb_k5ws_user:EhvieZAPy7XTc7b6Q13kyjLdhCZOv3xG@dpg-chogoevdvk4goeo0pheg-a.oregon-postgres.render.com/smartbraindb_k5ws
