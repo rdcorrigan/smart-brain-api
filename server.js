e86a08b3f280446d844951a2de135a243c33447f
Ryan Corrigan <ryandcorrigan@gmail.com>
2023-05-29 14:22:14 -0600

    reactivated particles bg

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
+        connection: {
         connectionString: process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false },
-      host : process.env.DATABASE_HOST,
-      port: 5432,
-      user : process.env.DATABASE_USER,
-      password : process.env.DATABASE_PW,
-      database : process.env.DATABASE_DB
+        host : process.env.DATABASE_HOST,
+        port: 5432,
+        user : process.env.DATABASE_USER,
+        password : process.env.DATABASE_PW,
+        database : process.env.DATABASE_DB
     }
 });
 
